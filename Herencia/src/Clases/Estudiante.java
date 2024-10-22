ca41997ffb63f39d91529ccf2694ae8b5d247bd4
Daniel cardenas <df028231@gmail.com>
2024-10-21 20:43:55 -0500

    agrego los otros trabajos q se han pedido

---
+++ Herencia/src/Clases/Estudiante.java
@@ -0,0 +1,22 @@
+public class Estudiante extends Persona {
+
+private String carrera; // propiedad
+private int legajo; // propiedad
+
+public Estudiante () {// constructor
+}
+
+public String getCarrera () {// asesor a carrera 
+    return carrera;
+}
+
+public void setCarrera (String val) { // mutador de carrera 
+    this.carrera val;
+}
+
+public void setCodigo(int codigo) {
+    this.codigo = codigo;
+}
+
+
+}
